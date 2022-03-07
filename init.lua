629bd4696c4c43f68914ea3646262ce7d10ca820
Peter <peterzhaozzy@gmail.com>
2022-03-06 20:22:26 -0800

    less error when installing

--- init.lua
+++ init.lua
@@ -3,6 +3,15 @@
 --    / // __ \/ / __/ / / / / / __ `/
 --  _/ // / / / / /__ / / /_/ / /_/ /
 -- /___/_/ /_/_/\__(_)_/\__,_/\__,_/
+
+-- LSP
+local fn = vim.fn
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+if not (fn.empty(fn.glob(install_path)) > 0) then
+  require('core.language-server-setup')
+  require'impatient'.enable_profile()
+end
+
 -- Plugins
 require('plugins')
 
@@ -12,11 +21,7 @@ require('core.settings').load_autocmds()
 require('core.keymappings')
 require('core.autocmds')
 
--- LSP
-require('core.language-server-setup')
-
 -- Color Scheme
 vim.g.colors_name = 'snappy'
 vim.cmd([[colorscheme snappy]])
 
-require'impatient'.enable_profile()
