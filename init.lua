5f381f1348173b71e72c20febc80e12ed40ecda9
Peter <peterzhaozzy@gmail.com>
2022-08-31 23:37:58 -0700

    better install

--- init.lua
+++ init.lua
@@ -10,19 +10,22 @@ local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if not (fn.empty(fn.glob(install_path)) > 0) then
   require('core.language-server-setup')
   -- require'impatient'.enable_profile()
+
+  -- Plugins
+  require('plugins')
+  vim.notify = require("notify")
+
+  -- Core settings
+  require('core.settings').load_options()
+  require('core.settings').load_autocmds()
+  require('core.keymappings')
+  require('core.autocmds')
+
+  -- Color Scheme
+  vim.g.colors_name = 'snappy'
+  vim.cmd([[colorscheme snappy]])
 end
 
 -- Plugins
 require('plugins')
-vim.notify = require("notify")
-
--- Core settings
-require('core.settings').load_options()
-require('core.settings').load_autocmds()
-require('core.keymappings')
-require('core.autocmds')
-
--- Color Scheme
-vim.g.colors_name = 'snappy'
-vim.cmd([[colorscheme snappy]])
 
