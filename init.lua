7b0a99b461f05c99b00ecc4f07fb9aea9cf4c737
Peter <peterzhaozzy@gmail.com>
2022-12-27 16:45:42 -0800

    bootstrap fix

--- init.lua
+++ init.lua
@@ -5,29 +5,26 @@
 -- /___/_/ /_/_/\__(_)_/\__,_/\__,_/
 
 -- LSP
-local fn = vim.fn
-local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-if not (fn.empty(fn.glob(install_path)) > 0) then
-  require('core.language-server-setup')
-  require'impatient'.enable_profile()
+require('core.language-server-setup')
+require'impatient'.enable_profile()
 
-  -- Plugins
-  require('plugins')
-  vim.notify = require("notify")
+-- Plugins
+require('plugins')
+vim.notify = require("notify")
 
-  -- Core settings
-  require('core.settings').load_options()
-  require('core.settings').load_autocmds()
-  require('core.keymappings')
-  require('core.autocmds')
+-- Core settings
+require('core.settings').load_options()
+require('core.settings').load_autocmds()
+require('core.keymappings')
+require('core.autocmds')
 
-  -- Color Scheme
-  vim.g.colors_name = 'snappy'
-  vim.cmd([[colorscheme snappy]])
-else
-  -- Plugins
-  vim.opt.termguicolors = true
-  require('plugins')
-  vim.notify("After installation, please restart Nvim 🥰.")
-end
+-- Color Scheme
+vim.g.colors_name = 'snappy'
+vim.cmd([[colorscheme snappy]])
+-- Plugins
 
+
+
+-- vim.opt.termguicolors = true
+-- require('plugins')
+-- vim.notify("After installation, please restart Nvim 🥰.")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -279,8 +279,8 @@ local function setupPackage()
   end)
 end
 
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.fn.system({
+if fn.empty(fn.glob(install_path)) > 0 then
+  fn.system({
     'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path
   })
   execute 'packadd packer.nvim'
